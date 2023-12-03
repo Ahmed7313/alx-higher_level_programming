de2086804b87f63aab70f1739b7ac21441effac6
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-12-03 14:29:51 -0800

    q100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,10 +3,7 @@
 def print_reversed_alphabet():
     alpha_string = ""
     for i in range(122, 96, -1):
-        if i % 2 == 0:  # Even ASCII values are uppercase (after conversion)
-            alpha_string += chr(i - 32)
-        else:  # Odd ASCII values are lowercase
-            alpha_string += chr(i)
-    print(alpha_string, end='')
+        alpha_string += chr(i - 32) if i % 2 == 0 else chr(i)
+    print("{}".format(alpha_string), end='')
 
 print_reversed_alphabet()
