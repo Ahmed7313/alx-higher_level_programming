7574b8940f66fd6493d149b90f45eebb93a8876a
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-12-03 14:06:59 -0800

    q3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-output = ""
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        output += chr(i)
-print(output, end='')
+output = []
+for i in range(97, 123):  # ASCII values for 'a' to 'z'
+    if i != 101 and i != 113:  # Excluding 'e' and 'q'
+        output.append(chr(i))
+print("{}".format(''.join(output)), end='')
