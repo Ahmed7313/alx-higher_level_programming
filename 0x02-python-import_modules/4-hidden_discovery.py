1b2abd3cbcffd3dc5d4c627823aea8143a5f5e9d
Ahmed7313 <ahmedrabie7313@gmail.com>
2023-12-06 15:58:23 -0800

    q4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+    names = sorted(name for name in dir(hidden_4) if not name.startswith("__"))
     print("\n".join(names))
