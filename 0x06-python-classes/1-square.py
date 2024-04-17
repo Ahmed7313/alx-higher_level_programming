2722d4572b2c8f96558c57397b9b1dcc9d11bef7
= <=>
2024-04-17 08:50:17 -0700

    q1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,18 @@
-#!/user/bin/python3
+#!/usr/bin/python3
+"""Defines a class Square"""
+
 
 class Square:
-    """Defins a square"""
+    """
+    Class that defines properties of square by: (based on 0-square.py).
 
+    Attributes:
+        size: size of a square (1 side).
+    """
     def __init__(self, size):
-        self.__size = size  
+        """Creates new instances of square (1 side).
 
+        Args:
+            size: size of the square.
+        """
+        self.__size = size
