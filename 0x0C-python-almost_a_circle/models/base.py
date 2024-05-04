d943b33844321154659713fe88ff840c2c34e56e
= <=>
2024-05-03 21:52:28 -0700

    q0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
+
 """Module for Base class."""
+
 
 class Base:
     """Base class for all future classes in the project.
-    The purpose of this class is to manage the id attribute in all future classes
-    and to avoid duplicating code and potential bugs.
+
+    The purpose of this class is to manage the id attribute in all future
+    classes and to avoid duplicating code and potential bugs.
     """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
         """Initialize a new Base instance.
+
         Args:
             id (int): The id of the object, or None for automatic assignment.
         """
@@ -18,4 +22,4 @@ class Base:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects 
+            self.id = Base.__nb_objects
