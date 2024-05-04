1cafcb58456d7adbe32d888c185a40bc3adb677a
= <=>
2024-05-03 23:00:14 -0700

    q15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-
 """Module for Base class."""
+import json
 
 
 class Base:
-    """Base class for all future classes in the project.
-
-    The purpose of this class is to manage the id attribute in all future
-    classes and to avoid duplicating code and potential bugs.
-    """
-
+    """Base class for all future classes in the project."""
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base instance.
-
-        Args:
-            id (int): The id of the object, or None for automatic assignment.
-        """
+        """Initialize a new Base instance."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
